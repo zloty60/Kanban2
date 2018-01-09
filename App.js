3ef6b53893ee95447c88ce3e619d4d743213dee7
zloty60 <przemek.bzowka@gmail.com>
2018-01-09 02:37:06 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,32 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': '2661',
+  'X-Auth-Token': '53ba875f15d5f4ec27bd5079e2e3fce8'
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function(response) {
+      setupColumns(response.columns);
+    }
+});
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+  		var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    	col.createCard(card);
+  	})
+}
